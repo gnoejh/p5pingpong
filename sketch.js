0426c5e03046d511b7496903b641dbd0967166f5
hjeong <hjeong@postech.ac.kr>
2021-04-08 23:32:16 +0500

    update

--- sketch.js
+++ sketch.js
@@ -3,12 +3,16 @@ let aiPaddle;
  
 function setup() {
   createCanvas(624, 351);
+
+  //TODO Paddle instances
   playerPaddle = new Paddle(26);
   aiPaddle = new Paddle(width - 48);
 }
  
 function draw() {
   background(0);
+
+  //TODO Draw Paddles
   playerPaddle.display();
   aiPaddle.display();
 }
